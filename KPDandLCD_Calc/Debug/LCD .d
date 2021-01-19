76b5adb9812a6366ae761f5f6dff96b93fa914f1
Kamal Magdy <68973035+kem0magdy@users.noreply.github.com>
2021-01-19 18:12:26 +0200

    Add files via upload

---
+++ KPDandLCD_Calc/Debug/LCD .d
@@ -0,0 +1,14 @@
+LCD\ .d LCD\ .o: ../LCD\ .c ../STD_TYPES.h ../BIT_MATH.h \
+  ../DIO_interface.h ../LCD_Interface.h ../LCD_private.h ../LCD_config.h
+
+../STD_TYPES.h:
+
+../BIT_MATH.h:
+
+../DIO_interface.h:
+
+../LCD_Interface.h:
+
+../LCD_private.h:
+
+../LCD_config.h:
